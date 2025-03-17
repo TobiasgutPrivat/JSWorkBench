8dceb7bed42d3af5c31493f9c6c259bb121192ac
TobiasgutPrivat <tobias.gut.03@gmail.com>
2025-03-17 10:51:23 +0100

    simplify base

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
+// src/App.tsx - a more compact version
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to My Simple React App</h1>
+      <Button variant="primary">Click Me!</Button>
     </div>
   );
 }
